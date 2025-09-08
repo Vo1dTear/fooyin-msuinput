a8ed6a70ccaee268452b39553ce09bd2917da0bb
Vo1dTear <54908155+Vo1dTear@users.noreply.github.com>
2025-09-08 07:26:35 -0300

    Fixed an oversight in some comments

--- prepare_msu.cmake
+++ prepare_msu.cmake
@@ -1,13 +1,13 @@
-# Copiar plugin
+# Copy plugin
 file(COPY "${MSUINPUT_SRC}" DESTINATION "${FOOYIN_SRC}/src/plugins")
 message(STATUS "Copied msuinput to ${FOOYIN_SRC}/src/plugins")
 
-# Leer el CMakeLists.txt de plugins
+# Read the plugins CMakeLists.txt
 file(READ "${PLUGINS_CMAKELISTS}" CONTENTS)
 
-# Añadir add_subdirectory(msuinput) solo si no existe
+# Add add_subdirectory(msuinput) only if it doesn't exist
 if(NOT "${CONTENTS}" MATCHES "add_subdirectory\\(msuinput\\)")
-    # Añadir al final
+    # Append at the end
     set(NEW_CONTENTS "${CONTENTS}\n# Added automatically by fooyin-msuinput\nadd_subdirectory(msuinput)\n")
     file(WRITE "${PLUGINS_CMAKELISTS}" "${NEW_CONTENTS}")
     message(STATUS "Added add_subdirectory(msuinput) to the end of ${PLUGINS_CMAKELISTS}")
